45532faea87151ae9b664c7a731f9b03cc752e2a
matecasaretto <fumet23@gmail.com>
2023-11-13 21:49:54 -0300

    Correcciones

--- src/componentes/cartitem/CartItem.jsx
+++ src/componentes/cartitem/CartItem.jsx
@@ -7,7 +7,7 @@ const CartItem = ({ id, titulo, precio, quantity, removeItem }) => {
     <div className='CartItem'>
       <div className='CartItemDetails'>
         <h3>{titulo}</h3>
-        <p>Precio: ${precio}</p>
+        <p>Precio ${precio}</p>
         <p>Cantidad: {quantity}</p>
       </div>
       <button onClick={() => removeItem(id)} className='RemoveButton'>Quitar</button>
